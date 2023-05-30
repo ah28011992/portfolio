c3a2c841884969c2cbf0c23697ea4b78d2a2036b
ah28011992 <hedgecockaaron@gmail.com>
2023-05-30 08:21:57 +0100

    add linking changes and styling to nav

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import Header from "../../components/Header/Header";
-import IconMenu from "../../components/UI/Menu";
+
 const Home = () => {
   return (
     <>
       <Header />
-      <IconMenu />
     </>
   );
 };
