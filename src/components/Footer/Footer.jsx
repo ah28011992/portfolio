4fc8f3d1633bce055c5c2a1860fe1ee0abf391a4
ah28011992 <hedgecockaaron@gmail.com>
2023-06-02 21:32:48 +0100

    add footer and styling to mobile

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./footer.module.css";
 const Footer = () => {
-    return (
-        <div>Footer</div>
-    )
-}
+  return (
+    <footer>
+      <section className={styles.wave_container}>
+        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 -10 1340 320'>
+          <path
+            fill='#e95420'
+            fillOpacity='1'
+            d='M0,32L30,69.3C60,107,120,181,180,186.7C240,192,300,128,360,96C420,64,480,64,540,80C600,96,660,128,720,144C780,160,840,160,900,138.7C960,117,1020,75,1080,64C1140,53,1200,75,1260,106.7C1320,139,1380,181,1410,202.7L1440,224L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z'></path>
+        </svg>
+      </section>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import Main_home from "./Components/Main_home";
-
+import Footer from "../../components/Footer/Footer";
 const Home = () => {
   return (
     <>
       <Header />
       <Main_home />
+      <Footer />
     </>
   );
 };
